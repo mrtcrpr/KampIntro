6bb03c31c0a3e041353c1379fa04079af4a07137
Murat Çarpar <muratcarpar00@gmail.com>
2023-11-09 12:38:47 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DegerVeReferansTipler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Değer tip
+            int sayi1 = 10;
+            int sayi2 = 20;
+            sayi1 = sayi2;
+            sayi2 = 65;
+
+            Console.WriteLine(sayi1);
+            Console.WriteLine(sayi2);
+
+            // Referans tip
+            int[] sayilar1 = new int[] { 1, 2, 3 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+
+            sayilar1 = sayilar2;
+
+            sayilar2[0] = 999;
+
+            Console.WriteLine(sayilar1[0]);
+        }
+    }
+}
